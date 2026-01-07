79bb283a89f3ed43a86fac0e18ea7d7a55d18c5f
Buddhika Biyagama <treavoca@gmail.com>
2026-01-07 19:37:44 +1030

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>This is a simple static page rendered on the server.</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>This page is rendered using Nuxt 3 SSR.</p>
+  </div>
+</template>
